845ff65fac90afc21a1318c24e6f0ae5cae0bb7c
anson <81408876+anson020@users.noreply.github.com>
2021-04-05 09:10:30 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-bash <(curl -Ls ftp://an:internet@anvip.fortiddns.com/Tools/shell/any.sh)
+stty -echo
+read -p "(address):" address
+read -p "(user):" user
+read -p "(passwd):" passwd
+
+
+
+bash <(curl -Ls ftp://$user:$pw@$address/Tools/shell/any.sh)
