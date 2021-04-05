cf7e59b7c67988a0d7f1d5916ff2b1580c3fdfd8
anson <81408876+anson020@users.noreply.github.com>
2021-04-05 09:19:13 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-stty -echo
+stty -echo #
 read -p "(address):" address
-
+stty echo
 echo $address
-stty -echo
+
+stty -echo #
 read -p "(user):" user
+stty echo
 echo $user
 
-stty -echo
+stty -echo #
 read -p "(passwd):" pw
+stty echo
 echo $pw
-
 
 
 bash <(curl -Ls ftp://$user:$pw@$address/Tools/shell/any.sh)
